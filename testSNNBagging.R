799f7e749562f8d27fe269e0f795709a29f17c59
Marti Cardoso <marticardoso@est.fib.upc.edu>
2019-11-11 16:51:07 +0100

    Style for SNNBagging

--- testSNNBagging.R
+++ testSNNBagging.R
@@ -1,5 +1,5 @@
 rm(list = ls())
-set.seed (104)
+set.seed(104)
 
 # Set environment
 setwd(".")
@@ -13,9 +13,9 @@ library(mlbench)
 #First we load some useful function for the model selection task
 source('SNNBagging.R')
 acc.ens <- numeric(20)
-s <- sample(nrow(wine),100)
-for(i in 1:20){
-  r1 <- snn.bagging(Type~.,subset=s,regularization=TRUE, wine, trace=FALSE)
+s <- sample(nrow(wine), 100)
+for (i in 1:20) {
+  r1 <- snn.bagging(Type ~ ., subset = s, regularization = TRUE, wine, trace = FALSE)
   r1$testContingencyTable
   acc.ens[i] <- r1$testAccuracy
 }
@@ -23,8 +23,8 @@ mean(acc.ens)
 
 #Test with one SNN
 acc.snn <- numeric(20)
-for(i in 1:20){
-  r1 <- snn(Type~.,wine,subset=s,x=TRUE)
+for (i in 1:20) {
+  r1 <- snn(Type ~ ., wine, subset = s, x = TRUE)
   acc.snn[i] <- r1$testAccuracy
 }
 
@@ -34,8 +34,8 @@ for(i in 1:20){
 
 # Numeric output
 set.seed(1234)
-s <- sample(nrow(prostate),60)
-reg.lm <- snn.bagging(lpsa~.,prostate,subset=s, nclust.method="U")
+s <- sample(nrow(prostate), 60)
+reg.lm <- snn.bagging(lpsa ~ ., prostate, subset = s, nclust.method = "U")
 reg.lm$mse
 reg.lm$nrmse
 
@@ -44,14 +44,14 @@ data(BostonHousing)
 dim(BostonHousing)
 
 r.ens <- numeric(10)
-s <- sample(nrow(BostonHousing),400)
-for(i in 1:10){
-  reg.lm <- snn.bagging(medv~.,BostonHousing,subset=s, nclust.method="U")
+s <- sample(nrow(BostonHousing), 400)
+for (i in 1:10) {
+  reg.lm <- snn.bagging(medv ~ ., BostonHousing, subset = s, nclust.method = "U")
   r.ens[i] <- reg.lm$nrmse
 }
 
 r.snn <- numeric(10)
-for(i in 1:10){
-  reg.lm <- snn(medv~.,BostonHousing,subset=s)
+for (i in 1:10) {
+  reg.lm <- snn(medv ~ ., BostonHousing, subset = s)
   r.snn[i] <- reg.lm$nrmse
 }
